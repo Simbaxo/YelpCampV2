821bad2b3766703c7b676dd948c79b9227514b12
Andres <ballerlikemike23@gmail.com>
2018-03-12 23:56:30 -0400

    Comments Post

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express    = require("express"),
       bodyParser = require("body-parser"),
       mongoose   = require("mongoose"),
       Campground = require("./models/campground"),
+      Comment    = require("./models/comment"),
       seedDB     = require("./seeds");
 
 mongoose.connect("mongodb://localhost/yelp_camp");
@@ -77,7 +78,29 @@ app.get("/campgrounds/:id/comments/new", function(req, res) {
       res.render("comments/new", {campground: campground});
     }
   });
-  res.render("comments/new");
+});
+
+app.post("/campgrounds/:id/comments", function(req, res) {
+  // lookup campground using ID
+  Campground.findById(req.params.id, function(err, campground) {
+    if(err) {
+      console.log(err);
+      res.redirect("/campgrounds");
+    } else {
+        Comment.create(req.body.comment, function(err, comment) {
+          if(err) {
+            console.log(err);
+          } else {
+            campground.comments.push(comment);
+            campground.save();
+            res.redirect('/campgrounds/' + campground._id);
+          }
+        })
+    }
+  });
+  // create new comment
+  // connect new comment to camgground
+  // redirect campground show page
 });
 
 // Starts server on Port: 3000

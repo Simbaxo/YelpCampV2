791b01ec6c891024f5fd74c64e541ec8059f6aaf
Andres <ballerlikemike23@gmail.com>
2018-03-13 19:00:37 -0400

    Custom Stylesheet

--- README.md
+++ README.md
@@ -62,3 +62,7 @@ Each Campground has:
 * Discuss nested routes
 * Add the comment new and create routes
 * Add the new comment form
+
+### Style Show Page
+* Add sidebar to show page
+* Display comments nicely
--- app.js
+++ app.js
@@ -9,6 +9,7 @@ const express    = require("express"),
 mongoose.connect("mongodb://localhost/yelp_camp");
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 seedDB();
 
 // Shows The Landing Page

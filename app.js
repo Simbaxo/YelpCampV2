82329d33abfa24bc816d08d6c61be80c6e2d3214
Andres <ballerlikemike23@gmail.com>
2018-03-27 00:42:33 -0400

    Updated Port

--- app.js
+++ app.js
@@ -50,6 +50,6 @@ app.use("/campgrounds", campgroundRoutes);
 app.use("/campgrounds/:id/comments", commentRoutes);
 
 // Starts server on Port: 3000
-app.listen(3000, function() {
-  console.log("The YelpCamp Server Has Started!");
+app.listen(process.env.PORT || 3000, function () {
+  console.log('app running at port 3000...');
 });

a649be32f559a2d4a454902ef4f960d33eca1a98
Andres <ballerlikemike23@gmail.com>
2018-03-29 19:38:11 -0400

    MomentJS Added

--- app.js
+++ app.js
@@ -26,6 +26,7 @@ app.use(express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
 app.use(flash());
 // seedDB(); // Seed the Database
+app.locals.moment = require('moment');
 
 // PASSPORT CONFIGURATION
 app.use(require("express-session")({
